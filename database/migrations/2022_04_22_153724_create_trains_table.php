3d7578b003722969836d5462974f8361804d1c63
Marco Corallo <94033528+marcocor25@users.noreply.github.com>
2022-04-22 17:47:07 +0200

    Creata tabella trains

--- database/migrations/2022_04_22_153724_create_trains_table.php
+++ database/migrations/2022_04_22_153724_create_trains_table.php
@@ -15,6 +15,15 @@ class CreateTrainsTable extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('azienda');
+            $table->string('stazione_di_partenza');
+            $table->string('stazione_di_arrivo');
+            $table->time('orario_di_partenza');
+            $table->time('orario_di_arrivo');
+            $table->string('codice_treno');
+            $table->integer('numero_carrozze');
+            $table->boolean('in_orario');
+            $table->boolean('cancellato');
             $table->timestamps();
         });
     }
